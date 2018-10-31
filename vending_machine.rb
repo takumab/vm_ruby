5a6827021c11af1f83107f80603cc989323cb7ce
Tacuma Bell <takumab@gmail.com>
2018-10-31 18:06:22 +0000

    Add coins

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,11 +1,22 @@
 class VendingMachine
-  attr_reader :products
+  attr_reader :products, :coins
   def initialize
     @products = {
         'A1' => { name: 'Cheetos', price: 50 },
         'B1' => { name: 'Snickers', price: 50 },
         'C1' => { name: 'Sprite', price: 100 },
         'D1' => { name: 'Sandwich', price: 150 }
+    }
+
+    @coins = {
+      1 => 1,
+      2 => 1,
+      5 => 1,
+      10 => 1,
+      20 => 1,
+      50 => 1,
+      100 => 1,
+      200 => 1
     }
   end
 end

d72a2922e978b257e092178ef5093469b5ec2b15
Tacuma Bell <takumab@gmail.com>
2018-10-31 17:54:11 +0000

    Add examples

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 require './vending_machine'
 
 RSpec.describe VendingMachine do
+
+  it "should take an initial load of products and change." do
+  end
+
+  it "should have change in denominations of 1p, 2p, 5p, 10p, 20p, 50p, £1, £2." do
+  end
+
+  it "should ensure item is selected and the appropriate amount of money is inserted" do
+  end
+
+  it "should ask for more money if insufficient funds have been inserted." do
+  end
+
+  it "should return the correct product." do
+  end
+
+  it "should also return change if too much money is inserted" do
+  end
+
+  it "should keep track of the products and change that it contains." do
+  end
 end

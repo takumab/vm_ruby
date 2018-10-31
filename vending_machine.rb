6df51153aba23f8e1830870c9d0bd4aaf199f213
Tacuma Bell <takumab@gmail.com>
2018-10-31 18:04:58 +0000

    Add products

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,2 +1,11 @@
 class VendingMachine
+  attr_reader :products
+  def initialize
+    @products = {
+        'A1' => { name: 'Cheetos', price: 50 },
+        'B1' => { name: 'Snickers', price: 50 },
+        'C1' => { name: 'Sprite', price: 100 },
+        'D1' => { name: 'Sandwich', price: 150 }
+    }
+  end
 end
